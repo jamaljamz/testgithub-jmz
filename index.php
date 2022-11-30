8d6ea407960c11f10a7d91d085323dad3516e0da
alpa05 <algianpang@gmail.com>
2022-11-30 10:51:10 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,6 +4,9 @@
     </head>
     <body>
         <h1>Algian Ganteng</h1>
+        <body class="form">
+            <h1>JamsJums</h1>
+        </body>
         <div style="font-size: x-large;color: lightgreen;">Test Github</div>
     </body>
 </html>
